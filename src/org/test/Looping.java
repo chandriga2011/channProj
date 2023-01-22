edfa5fc90dbd18f3aabb9a356cf835070bbe2be8
chandriga2011 <chandrigads@gmail.com>
2023-01-22 23:56:58 +0530

    new code accepted

---
+++ src/org/test/Looping.java
@@ -0,0 +1,14 @@
+package org.test;
+
+public class Looping {
+	
+	public static void main(String[] args) {
+		int a=20;
+		if(a>=5) {
+			System.out.println("A is greater than 5");
+		}
+			
+		}
+	}
+
+
